d05d4a5c54f073adb2206670973ae42f7b9ffe97
urpit <patelurpit123@gmail.com>
2024-01-10 10:03:18 -0500

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities;
 
@@ -10,4 +11,22 @@ public class AppUser
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+
+    public DateOnly DateofBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Photo> photos { get; set; } = new();
+
+    // public int GetAge()
+    // {
+    //     return DateofBirth.CalculateAge();
+    // }
 }
+
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>()
+        .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.photos.FirstOrDefault(x => x.IsMain).Url))
+        .ForMember(desk => desk.Age, opt => opt.MapFrom(src => src.DateofBirth.CalculateAge()));
+        CreateMap<Photo, PhotoDto>();
+    }
+}
